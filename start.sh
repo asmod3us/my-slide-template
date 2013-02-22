39bf561b507e138702b650eb9cedd39582d45ea3
asmod3us <4sm0d3us@gmail.com>
2013-02-22 12:50:46 +0100

    increased timeout before launching browser

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ function webserver() {
 	local port="${1:-8000}"
 	# Set the default Content-Type to `text/plain` instead of `application/octet-stream`
 	# And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
-	sleep 0.5 & open "http://localhost:8000/" &
+	sleep 0.8 & open "http://localhost:8000/" &
 	python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port" 2>&1 > /dev/null
 }
 
